b5ad7425f48032b86259969e4128f68b8e27a212
SAVITHA J.B <saviisavitha649@gmail.com>
2026-01-22 13:12:25 +0530

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        float num1=sc.nextFloat();
+        System.out.println(num);
+        System.out.printf("%.2f",num1);
+        sc.close();
+    }
+}

00ee4102ede5b81a1452fbdf2a15b30ab775a36f
SAVITHA J.B <saviisavitha649@gmail.com>
2026-01-22 20:46:41 +0530

    Create Wordwithstatement.java

---
+++ Wordwithstatement.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class  Wordwithstatement {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("May I know how to learn C!!!...");
+    }
+}

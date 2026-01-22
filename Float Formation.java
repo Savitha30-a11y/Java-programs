3c6a5aecea82d5483075328612526e3855c3dc38
SAVITHA J.B <saviisavitha649@gmail.com>
2026-01-22 20:37:33 +0530

    Create Float Formation.java to format float output

---
+++ Float Formation.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public Float Formation {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner sc=new Scanner(System.in);
+        float num=sc.nextFloat();
+        System.out.printf("%.6f\n",num);
+        System.out.printf("%.4f\n",num);
+        System.out.printf("%.2f\n",num);
+        System.out.printf("%.0f\n",num);
+    }
+}
